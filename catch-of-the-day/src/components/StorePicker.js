d17b4bbe2921c93fdda1796b560e7cb44f9398bc
Praveen <spraveenitpro@gmail.com>
2022-03-14 22:20:05 -0700

    E-commerce site scaffolding

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 function StorePicker() {
-    return <p>Hello! World!!! Hello !!!!</p>
+    return (
+        <>
+            <form className="store-selector">
+                <h2>Please enter store name:</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store!</button>
+            </form>
+        </>
+    )
 }
 
 export default StorePicker
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,5 +2,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import StorePicker from './components/StorePicker'
+import './css/style.css'
 
 ReactDom.render(<StorePicker />, document.querySelector('#main'))
