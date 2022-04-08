00091a39b1a94ca512bfac008c063b56397c51bb
Praveen <spraveenitpro@gmail.com>
2022-04-08 16:56:34 -0700

    Starting Feedback app scaffold

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = "Blog post"
+    const body = " This is a blog post"
+    const comments = [
+        {id: 1, text: 'comment 1'},
+        {id: 2, text: 'comment 2'},
+        {id: 3, text: 'comment 3'},
+    ]
+
+    return (
+        <div className="container">
+            <h2>{title}</h2>
+            <p>{body}</p>
+            <div className="comments">
+                <h3>Comments {comments.length}</h3>
+                <ul>
+                    {comments.map((comment)=> (
+                         <li key={comment.id}>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
