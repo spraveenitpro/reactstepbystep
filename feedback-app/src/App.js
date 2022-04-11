42375686ef5f77773903953cf1347a216bab28dc
Praveen <spraveenitpro@gmail.com>
2022-04-11 09:18:41 -0700

    setup flow of props

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,31 +1,17 @@
-function App() {
-    const title = "Blog post"
-    const body = " This is a blog post"
-    const comments = [
-        {id: 1, text: 'comment 1'},
-        {id: 2, text: 'comment 2'},
-        {id: 3, text: 'comment 3'},
-    ]
-   
-    const showComments = true
-    const commentBlock =(
-        <div className="comments">
-            <h3>Comments {comments.length}</h3>
-            <ul>
-                {comments.map((comment)=> (
-                    <li key={comment.id}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>
-    )
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackData from './data/FeedbackData'
+import FeedbackList from './components/FeedbackList'
 
+function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
     return (
-        <div className="container">
-            <h2>{title}</h2>
-            <p>{body}</p>
-            {showComments && commentBlock}
-           
-        </div>
+        <>
+            <Header />
+            <div className="container">
+                <FeedbackList feedback={feedback} />
+            </div>
+        </>
     )
 }
 
